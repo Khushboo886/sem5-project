4319f717c9c2e2d8e7d09b819d336a4f2180c05c
Dipak Dhariyaparmar <dipak.parmaar@stellarmind.ai>
2026-03-03 16:50:07 +0530

    Updated DB to use enviornment variables

--- includes/db.php
+++ includes/db.php
@@ -1,13 +1,10 @@
 <?php
-/* ============================================
-   CloudConnect SQLite Database (Azure Free)
-============================================ */
 
 try {
     $db = new PDO(
-        "mysql:host=localhost;dbname=CloudConnect_db;charset=utf8mb4",
+        "mysql:host=localhost;dbname=" . getenv('DB_NAME') . ";charset=utf8mb4",
         getenv('DB_USER'),
-        getenv('DB_PASSWORD'),
+        getenv('DB_PASS'),
         [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
@@ -16,3 +13,4 @@ try {
 } catch (PDOException $e) {
     error_log("DB ERROR: " . $e->getMessage());
     die("Database connection failed.");
+}
